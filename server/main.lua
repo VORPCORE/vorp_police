5b9a96f1696005d18074622dcf72e5e15eaeec7a
Outsider <87246847+outsider31000@users.noreply.github.com>
2025-09-16 17:18:43 +0100

    fix imports

--- server/main.lua
+++ server/main.lua
@@ -1,4 +1,4 @@
-local LIB                 = Import("/configs/config", "/languages/translation", "logs")
+local LIB                 = Import({ "/configs/config", "/languages/translation", "/configs/logs" })
 local Config <const>      = LIB.Config --[[@as vorp_police_config]]
 local Translation <const> = LIB.Translation --[[@as vorp_police_translation]]
 local Logs <const>        = LIB.Logs
